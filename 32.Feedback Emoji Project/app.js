12cf0db01c78a58bc68a6f0cc41f8cba6217951b
Nayden Banchevski <163778420+NaydenBanchevski@users.noreply.github.com>
2024-07-18 19:06:55 -0500

    Feedback Emoji Project Final Commit

---
+++ 32.Feedback Emoji Project/app.js
@@ -0,0 +1,32 @@
+const ratings = document.querySelectorAll(".rating");
+const ratingsContainer = document.querySelector(".rating-container");
+const sendBtn = document.querySelector(".btn");
+const panel = document.querySelector("#panel");
+
+console.log(panel);
+
+let selectedRating = "Satisfied";
+
+ratingsContainer.addEventListener("click", (e) => {
+  if (e.target.parentNode.classList.contains("rating")) {
+    removeActives();
+    e.target.parentNode.classList.add("active");
+    selectedRating = e.target.nextElementSibling.innerHTML;
+  }
+});
+
+sendBtn.addEventListener("click", () => {
+  panel.innerHTML = `
+    <p class="heart">❤️</p>
+    <strong>Thank you!</strong>
+    <br>
+    <strong>Your Rating: ${selectedRating}</strong>`;
+});
+
+function removeActives() {
+  for (let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove("active");
+  }
+}
+
+console.log(ratingsContainer);
